a5d9e08124e98100a3ad0802e56e101e0256e506
kiko <kikopesqueira96@gmail.com>
2021-01-26 22:24:05 -0300

    crud farmacia

---
+++ projetos/loja/loja/src/main/java/com/farmacia/loja/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.farmacia.loja.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farmacia.loja.model.Produto;
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Long> {
+//public List<Produto > findAllbyProdutoContainingIgnoreCase(String produto);
+}
